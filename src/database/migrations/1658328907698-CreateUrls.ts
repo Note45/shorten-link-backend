12705b37bf5da43cefa184d2e8c8aaa84e47ef03
Note45 <antonion.2048@gmail.com>
2022-07-25 05:56:52 -0300

    feat: add CreateUrl migration

---
+++ src/database/migrations/1658328907698-CreateUrls.ts
@@ -0,0 +1,44 @@
+import {
+  MigrationInterface, 
+  QueryRunner, 
+  Table
+} from "typeorm";
+
+export class CreateUrls1658328907698 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'urls',
+        columns: [
+          {
+            name: 'id',
+            type: "uuid", 
+            isPrimary: true
+          },
+          {
+            name: "originalUrl",
+            type: "varchar",
+          },
+          {
+            name: "shortedUrl",
+            type: "varchar",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ]
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('urls');
+  }
+}
