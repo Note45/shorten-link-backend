129edef7151084a04b7b732fee7ad32da8e7307a
Note45 <antonion.2048@gmail.com>
2022-07-25 05:49:01 -0300

    fix: typeorm Url entity not working

--- src/entities/Url.ts
+++ src/entities/Url.ts
@@ -1,13 +1,39 @@
 import { ObjectType, Field, ID } from "type-graphql";
+import { 
+  Column, 
+  CreateDateColumn, 
+  Entity, 
+  PrimaryGeneratedColumn, 
+  UpdateDateColumn 
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
 
 @ObjectType({ description: 'The URL model' })
+@Entity("urls")
 export class Url {
   @Field(() => ID)
-  id: string;
+  @PrimaryGeneratedColumn()
+  id?: string;
+
+  @Column()
+  @Field(() => String)
+  originalUrl: string; 
+
+  @Column()
+  @Field(() => String)
+  shortedUrl: string;
 
   @Field(() => String)
-  originalUrl: String; 
+  @CreateDateColumn()
+  created_at: Date;
 
   @Field(() => String)
-  shortedUrl: String;
+  @UpdateDateColumn()
+  update_at: Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuidV4();
+    }
+  }
 }
