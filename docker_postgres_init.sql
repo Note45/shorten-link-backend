74cfdeddd42111370f48f7022fe641c7e1ed7164
Note45 <antonion.2048@gmail.com>
2022-08-30 11:35:41 -0300

    chore(docker): add script to create test database

---
+++ docker_postgres_init.sql
@@ -0,0 +1,9 @@
+
+CREATE DATABASE "shorten-link-test"
+    WITH 
+    OWNER = docker
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
