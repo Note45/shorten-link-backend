1d39a7e26ae5bd57d0bc7c10f244a076c97f347f
Note45 <antonion.2048@gmail.com>
2022-09-08 14:39:48 -0300

    test(tests/unit/url): add unit tests to url entities

---
+++ tests/unit/url/entities/url.entitie.spec.ts
@@ -0,0 +1,21 @@
+import { Url } from '../../../../src/modules/url/infra/typeorm/entities/Url';
+
+describe('[MODULE/URL/INFRA/TYPEORM/ENTITIES]', () => {
+  describe('[HAPPY-PATH]', () => {
+    it('should be able to create url entities with defined id', async () => {
+      const url = new Url();
+      const urlMock = {
+        originalUrl: 'http://test-url.com.br',
+        shortenUrl: '0.com',
+        id: '0166d67c-86d7-46d2-9181-8418128e47ad',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      };
+
+      Object.assign(url, urlMock);
+
+      expect(url.id).toBeDefined();
+      expect(url).toEqual(urlMock);
+    });
+  });
+});
